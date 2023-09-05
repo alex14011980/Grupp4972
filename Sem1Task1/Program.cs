1079bde652d41d2358a26050325082a9d810026c
Alexander Bedrin <alexton83@mail.ru>
2023-09-05 16:29:00 +0400

    add seminar 1

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №1.
+// Напишите программу, которая на вход принимает два числа и
+// проверяет является ли первое число квадратом второго.
+
+int numA = int.Parse(Console.ReadLine()??"0");
+int numB = int.Parse(Console.ReadLine()??"0");
+if(numA * numA == numB)
+{
+    Console.WriteLine("Первое число квадрат второго ");
+
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,8 @@
+﻿int numN = int.Parse(Console.ReadLine()??"0");
+int evnumN = (-1) * numN;
+while (evnumN < numN)
+{
+  Console.Write(evnumN + ",");
+  evnumN = evnumN + 1;  
+}
+Console.WriteLine(numN);
