49f13fb083db3ba334bb6d1a6de8ea28bd480d18
Alexander Bedrin <alexton83@mail.ru>
2023-09-10 23:26:56 +0400

    add seminar 2.1

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача №13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// * Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+
+// Console.WriteLine("Введите число: ");
+// int numbers = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введенное число - " + numbers);
+// char[] digitChar = numbers.ToString().ToCharArray();
+// Console.WriteLine(numbers < 100 ? "Третьей цифры нет" :
+// digitChar[2] + " Третья цифра числа");
+
+
+
+Console.WriteLine("Введите число: ");                   // Пользовательский ввод
+int number = int.Parse(Console.ReadLine()??"0");         // Назначаем переменную
+
+if(number < 100)                                            // Проверяем на наличие третьей цифры
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if(number < 1000)                                               // Выводим третью цифру. и. т.д. до 10 знаков
+{
+    number =number %10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+}
+if(number < 10000) 
+{
+    number = (number/10)%10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+} 
+if(number < 100000) 
+{
+    number = (number/100)%10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+}
+if(number < 1000000) 
+{
+    number = (number/1000)%10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+}
+if(number < 10000000) 
+{
+    number = (number/10000)%10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+}
+if(number < 100000000) 
+{
+    number = (number/100000)%10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+}
+if(number < 1000000000) 
+{
+    number = (number/1000000)%10;
+    Console.WriteLine(number + " - Третья цифра числа"); 
+}
+
+
+
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача №15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+// int day = int.Parse(Console.ReadLine()??"0");
+
+// string[] dayOfWeek = new string [7];
+// dayOfWeek[0] = "Понедельник";
+// dayOfWeek[1] = "Вторник";
+// dayOfWeek[2] = "Среда";
+// dayOfWeek[3] = "Четверг";
+// dayOfWeek[4] = "Пятница";
+// dayOfWeek[5] = "Суббота";
+// dayOfWeek[6] = "Воскресенье";
+
+// if (day > 5)
+// {
+//     Console.WriteLine(dayOfWeek[day - 1 ] + " - Выходной день");
+// }
+// else 
+// {
+//     Console.WriteLine(dayOfWeek[day - 1 ] + " - Рабочий день");
+// }
+
+
+// int day = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Введите номер дня недели: ");                        // Пользовательский ввод
+var day = int.Parse(Console.ReadLine()??"0");                             // Назначаем переменную
+Dictionary<string, int> Directory = new Dictionary<string, int>();        //Cоздаем словарь Dictionary  
+var dayOfWeeky = new Dictionary<int, string>()
+{
+    [1] = "Понедельник",
+    [2] = "Вторник",
+    [3] = "Среда",
+    [4] = "Четверг",
+    [5] = "Пятница",
+    [6] = "Суббота",
+    [7] = "Воскресенье"   
+ 
+}; 
+
+if(day <= 5)                                                                //Проверяем на наличие рабочие дни 
+{
+    Console.WriteLine(" - Нет");                                             //Выводим сообщение
+
+}
+else if(day <=7)                                                               // Проверяем на наличие выходного дня
+{
+    Console.WriteLine(" - Да");                                                 // Выводим сообщение
+}
+else
+{
+    Console.WriteLine("Не является днем недели");                               // проверка на неверный ввод. Выводим сообщение.
+}
