b454ffd2db0c55035c9e5c0b3db930076bde04e5
Alexander Bedrin <alexton83@mail.ru>
2023-09-11 19:54:10 +0400

    изменение кода

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -21,41 +21,41 @@ if(number < 100)                                            // Проверяе�
 {
     Console.WriteLine("Третьей цифры нет");
 }
-if(number < 1000)                                               // Выводим третью цифру. и. т.д. до 10 знаков
+else if(number < 1000)                                               // Выводим третью цифру. и. т.д. до 10 знаков
 {
-    number =number %10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+    number = number %10;
+    
 }
-if(number < 10000) 
+else if(number < 10000) 
 {
     number = (number/10)%10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+    
 } 
-if(number < 100000) 
+else if(number < 100000) 
 {
     number = (number/100)%10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+    
 }
-if(number < 1000000) 
+else if(number < 1000000) 
 {
     number = (number/1000)%10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+     
 }
-if(number < 10000000) 
+else if(number < 10000000)
 {
     number = (number/10000)%10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+     
 }
-if(number < 100000000) 
+else if(number < 100000000) 
 {
     number = (number/100000)%10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+     
 }
-if(number < 1000000000) 
+else if (number < 1000000000) 
 {
     number = (number/1000000)%10;
-    Console.WriteLine(number + " - Третья цифра числа"); 
+    
 }
 
-
+Console.WriteLine(number + " - Третья цифра числа"); 
 
