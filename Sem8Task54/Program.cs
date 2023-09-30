9f63db94f102010ac647ce18b7e3f5a8f483c041
Alexander Bedrin <alexton83@mail.ru>
2023-09-30 20:32:05 +0400

    Seminar 8

---
+++ Sem8Task54/Program.cs
@@ -0,0 +1,78 @@
+﻿// // Задача 54:
+// // Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+// // каждой строки двумерного массива.
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // В итоге получается вот такой массив:
+// // 7 4 2 1
+// // 9 5 3 2
+// // 8 4 4 2
+
+int[,] table = new int[3, 4]; //размер массива
+Fill2DArray(table);  // рандомное заполнение массива
+PrinArray(table);  // вывод на экран сгенерированного массива
+SortRow2Array(table); // сортировка массива
+Console.WriteLine();
+PrintSort2Array(table); // вывод сортированного архива
+
+
+// Функция заполнения массива рандомно числами от 1 до 9
+void Fill2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+//вывод сгенерированного массива
+void PrinArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Функция сортировки элементов в строке двумерного массива, по убыванию
+void SortRow2Array(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Функция вывода двумерного массива
+void PrintSort2Array(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ 
