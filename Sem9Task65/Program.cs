6b296031ab53ff02607a769aa74b9021d0aea311
Alexander Bedrin <alexton83@mail.ru>
2023-10-01 00:35:46 +0400

    Seminar9

---
+++ Sem9Task65/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 65
+// Задайте значения M и N. Напишите программу, которая найдёт все натуральные элементы в промежутке от M до N.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Формирование строки с числами
+string SearchForNaturalElementsRec (int m, int n)
+{
+    return n >= m ? $"{m} " + SearchForNaturalElementsRec(m + 1, n) : "";
+}
+
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+
+Console.WriteLine((m>n)?SearchForNaturalElementsRec(n, m):SearchForNaturalElementsRec(m, n));
